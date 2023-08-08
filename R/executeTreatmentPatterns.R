0000a5e72399031d871655be8a512192234848f2
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2023-08-08 15:04:09 +0200

    Updated docs

--- R/executeTreatmentPatterns.R
+++ R/executeTreatmentPatterns.R
@@ -41,7 +41,7 @@
 #'   
 #'   cohorts <- data.frame(
 #'     cohortId = c(1, 2, 3, 4, 5),
-#'     cohortName = c("ViralSinusitis", "Acetaminophen", "Aspirin", "Clavulanate" "Death"),
+#'     cohortName = c("ViralSinusitis", "Acetaminophen", "Aspirin", "Clavulanate", "Death"),
 #'     type = c("target", "event", "event", "event", "exit")
 #'   )
 #'   
