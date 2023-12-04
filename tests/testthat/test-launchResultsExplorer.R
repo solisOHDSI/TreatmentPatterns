5447cfa177931ce3e0507ba715661744fe19151e
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2023-12-04 15:31:29 +0100

    Added more tests

--- tests/testthat/test-launchResultsExplorer.R
+++ tests/testthat/test-launchResultsExplorer.R
@@ -3,7 +3,7 @@ library(TreatmentPatterns)
 
 test_that("launchResultsExplorer", {
   app <- launchResultsExplorer()
-  testthat::expect_s3_class(app, "shiny.appobj")
+  expect_s3_class(app, "shiny.appobj")
   
   rm(app)
   gc()
