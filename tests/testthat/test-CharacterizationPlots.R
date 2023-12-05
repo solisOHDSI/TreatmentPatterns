d67d820be8335b243f3608f4eecb56d26b20367d
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2023-12-05 10:09:13 +0100

    Added CharacterizationPlots tests

---
+++ tests/testthat/test-CharacterizationPlots.R
@@ -0,0 +1,52 @@
+library(testthat)
+library(R6)
+library(shiny)
+library(TreatmentPatterns)
+
+test_that("CharacterizationPlots", {
+  characterizationPlots <- CharacterizationPlots$new("app")
+  expect_true(is.R6(characterizationPlots))
+})
+
+test_that("UI", {
+  characterizationPlots <- CharacterizationPlots$new("app")
+  
+  expect_s3_class(characterizationPlots$uiBody(), "shiny.tag")
+  expect_s3_class(characterizationPlots$uiMenu(), "shiny.tag")
+})
+
+
+test_that("server", {
+  moduleInteractivePlots <- function(id, inputHandler, characterizationPlots) {
+    shiny::moduleServer(id, function(input, output, session) {
+      inputHandler$setDataPath(input = input, path = NULL)
+      
+      path <- system.file(package = "TreatmentPatterns", "DummyOutput", "output.zip")
+      
+      session$setInputs(
+        uploadField = list(
+          datapath = path,
+          name = "output.zip"
+        )
+      )
+      
+      inputHandler$server(input, output, session)
+      characterizationPlots$server(input, output, session, inputHandler)
+      
+      shiny::uiOutput(shiny::NS(id, "charAgePlot"))
+      shiny::uiOutput(shiny::NS(id, "charSexPlot"))
+      shiny::uiOutput(shiny::NS(id, "charIndexYearPlot"))
+    })
+  }
+  
+  shiny::testServer(
+    app = moduleInteractivePlots,
+    args = list(
+      inputHandler = InputHandler$new("app"),
+      characterizationPlots = CharacterizationPlots$new("app")), {
+        expect_s3_class(output$charAgePlot$html, "html")
+        expect_s3_class(output$charSexPlot$html, "html")
+        expect_s3_class(output$charIndexYearPlot$html, "html")
+      }
+  )
+})
