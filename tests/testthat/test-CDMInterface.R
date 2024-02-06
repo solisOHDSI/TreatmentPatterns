b01506d14ed1fd6a0ae78b85fd3868a3dd09d127
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2024-02-06 10:57:32 +0100

    Added extra test for fetching

---
+++ tests/testthat/test-CDMInterface.R
@@ -0,0 +1,66 @@
+library(testthat)
+library(TreatmentPatterns)
+library(dplyr)
+
+test_that("fetchCohortTable", {
+  cg <- generateCohortTableCG()
+  cdmc <- generateCohortTableCDMC()
+  
+  aCG <- Andromeda::andromeda()
+  aCDMC <- Andromeda::andromeda()
+  
+  dbcInterface <- TreatmentPatterns:::CDMInterface$new(
+    connectionDetails = cg$connectionDetails,
+    cdmSchema = "main",
+    resultSchema = "main"
+  )
+  
+  cdmcInterface <- TreatmentPatterns:::CDMInterface$new(
+    cdm = cdmc$cdm
+  )
+  
+  minEraDuration <- 120
+  
+  dbcInterface$fetchCohortTable(
+    cohorts = cg$cohorts,
+    cohortTableName = cg$cohortTableName,
+    andromeda = aCG,
+    andromedaTableName = cg$cohortTableName,
+    minEraDuration = minEraDuration
+  )
+  
+  cdmcInterface$fetchCohortTable(
+    cohorts = cdmc$cohorts,
+    cohortTableName = cdmc$cohortTableName,
+    andromeda = aCDMC,
+    andromedaTableName = cdmc$cohortTableName,
+    minEraDuration = minEraDuration
+  )
+  
+  # Check nRows
+  expect_identical(
+    aCG$cohort_table %>% collect() %>% nrow(),
+    aCDMC$cohort_table %>% collect() %>% nrow()
+  )
+  
+  # check n > 1 treatments
+  expect_identical(
+    aCG$cohort_table %>% group_by(.data$subject_id) %>%
+      summarize(n = n()) %>% filter(n > 1) %>% collect(),
+    aCDMC$cohort_table %>% group_by(.data$subject_id) %>%
+      summarize(n = n()) %>% filter(n > 1) %>% collect()
+  )
+  
+  # check n == 1 treatments
+  expect_identical(
+    aCG$cohort_table %>% group_by(.data$subject_id) %>%
+      summarize(n = n()) %>% filter(n == 1) %>% collect(),
+    aCDMC$cohort_table %>% group_by(.data$subject_id) %>%
+      summarize(n = n()) %>% filter(n == 1) %>% collect()
+  )
+  
+  dbcInterface$disconnect()
+  Andromeda::close(aCG)
+  Andromeda::close(aCDMC)
+  DBI::dbDisconnect(cdmc$con, shutdown = TRUE)
+})
